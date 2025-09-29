8b309166080505e548ddacacddbca99805977570
vivixieka <vivixieka@163.com>
2025-09-29 14:56:57 +0200

    exercise1.7

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -7,6 +7,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = () => good + neutral + bad
+  const average = () => (good*1+ neutral*0 + bad*(-1))/(good + neutral + bad)
+  const positive = () => good / (good + neutral + bad) * 100
 
   return (
     <div>
@@ -15,9 +18,12 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      good {good} <br />
+      neutral {neutral} <br />
+      bad {bad} <br />
+      all {all()} <br />
+      average {average()} <br />
+      positivie {positive()} %
     </div>
   )
 }
