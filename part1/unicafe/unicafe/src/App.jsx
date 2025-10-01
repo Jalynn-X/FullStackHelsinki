c2f970c4107bb786f9d0221fca3b901456aaed66
vivixieka <vivixieka@163.com>
2025-10-01 17:17:33 +0200

    exercise1.8

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
 
 // Note that your application needs to work only during a single browser session. Once you refresh the page, the collected feedback is allowed to disappear.
+const Statistics = (props) => {
+  const all = () => props.good + props.neutral + props.bad
+  const average = () => (props.good * 1 + props.neutral * 0 + props.bad * -1) / all()
+  const positive = () => props.good / (props.good + props.neutral + props.bad) * 100
+
+  return (
+    <div>
+    <h1>statistics</h1>
+    good {props.good} <br />
+    neutral {props.neutral} <br />
+    bad {props.bad} <br />
+    all {all()} <br />
+    average {average()} <br />
+    positivie {positive()} %
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = () => good + neutral + bad
-  const average = () => (good*1+ neutral*0 + bad*(-1))/(good + neutral + bad)
-  const positive = () => good / (good + neutral + bad) * 100
+
 
   return (
     <div>
@@ -17,13 +32,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      good {good} <br />
-      neutral {neutral} <br />
-      bad {bad} <br />
-      all {all()} <br />
-      average {average()} <br />
-      positivie {positive()} %
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
