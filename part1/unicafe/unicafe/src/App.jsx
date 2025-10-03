0027da61832f2f199532be179927e34483903ee2
vivixieka <vivixieka@163.com>
2025-10-03 22:00:02 +0200

    exercise1.10

--- part1/unicafe/unicafe/src/App.jsx
+++ part1/unicafe/unicafe/src/App.jsx
@@ -1,6 +1,16 @@
 import { useState } from 'react'
 
 // Note that your application needs to work only during a single browser session. Once you refresh the page, the collected feedback is allowed to disappear.
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const StatisticLine = ( {text, value} ) => (<div>{text} {value}</div>)
+
 const Statistics = (props) => {
   const all = () => props.good + props.neutral + props.bad
   const average = () => (props.good * 1 + props.neutral * 0 + props.bad * -1) / all()
@@ -16,12 +26,12 @@ const Statistics = (props) => {
 
   return (
     <div>
-    good {props.good} <br />
-    neutral {props.neutral} <br />
-    bad {props.bad} <br />
-    all {all()} <br />
-    average {average()} <br />
-    positivie {positive()} %
+      <StatisticLine text='good' value={props.good} />
+      <StatisticLine text='neutral' value={props.neutral} />
+      <StatisticLine text='bad' value={props.bad} />
+      <StatisticLine text='all' value={all()} />
+      <StatisticLine text='average' value={average()} />
+      <StatisticLine text='positive' value={positive() + '%'} />     
     </div>
   )
 }
@@ -36,9 +46,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)} text='good' />
+      <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button onClick={() => setBad(bad + 1)} text='bad' />
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
