01970ccf7c4207164841542b92467f0fbb766112
vivixieka <vivixieka@163.com>
2025-11-07 11:08:09 +0100

    exercise2.3

--- part2/courseinfo/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/courseinfo/src/components/Content.jsx
@@ -1,11 +1,15 @@
 import Part from "./Part"
 
 const Content = ( {content} ) => {
+  const total = content.reduce((sum, item) =>
+    sum + item.exercises, 0)
+
   return (
     <div>
     {content.map(part => 
       <Part key={part.id} part={part}/>
     )}
+    <p><b>total of {total} exercises</b></p>
     </div>
   )
 }
